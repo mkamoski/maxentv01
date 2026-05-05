c2de3ccf34e1a9787b2eb0067744c162d566a9a0
Mark Kamoski <mkamoski@users.noreply.github.com>
2026-05-05 07:43:42 -0400

    Fixed web app so it loads. (#19)

--- MaxEnt.Web/Program.cs
+++ MaxEnt.Web/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using MaxEnt.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
 
 await builder.Build().RunAsync();
